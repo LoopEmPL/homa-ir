1d24a3fc6b5d245e97eefe1c83ec97d707731ae8
LoopEmPL <39483525-LoopEmPL@users.noreply.replit.com>
2025-02-13 12:01:19 +0000

    User checkpoint: Add new Spring Boot application for HOMA-IR Calculator.

---
+++ HOMA-IR-Calculator/src/main/java/LoopEmPL/HOMA_IR/Calculator/HomaIrCalculatorApplication.java
@@ -0,0 +1,13 @@
+package LoopEmPL.HOMA_IR.Calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomaIrCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomaIrCalculatorApplication.class, args);
+	}
+
+}
---
+++ HOMA-IR-Calculator/src/test/java/LoopEmPL/HOMA_IR/Calculator/HomaIrCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package LoopEmPL.HOMA_IR.Calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomaIrCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
